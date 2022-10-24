6c335cf4121c98d0988ed0fe20bf02d3dda94584
fadliAyi <m.fadli4545@gmail.com>
2022-10-25 01:22:58 +0800

    create server

---
+++ main.go
@@ -0,0 +1,39 @@
+package main
+
+import (
+	"go-restful-api/app"
+	"go-restful-api/controller"
+	"go-restful-api/helper"
+	"go-restful-api/repository"
+	"go-restful-api/service"
+	"net/http"
+
+	_ "github.com/go-sql-driver/mysql" // dont forger to import driver
+
+	"github.com/go-playground/validator/v10"
+	"github.com/julienschmidt/httprouter"
+)
+
+func main() {
+
+	validate := validator.New()
+	db := app.NewDB()
+	categoryRepository := repository.NewCategoryReposotry()
+	categoryService := service.NewCategoryService(categoryRepository, db, validate)
+	categoryController := controller.NewCategoryController(categoryService)
+
+	router := httprouter.New()
+	router.GET("/api/categories", categoryController.FindAll)
+	router.GET("/api/categories/:categoryId", categoryController.FindById)
+	router.POST("/api/categories", categoryController.Create)
+	router.PUT("/api/categories/:categoryId", categoryController.Update)
+	router.DELETE("/api/categories/:categoryId", categoryController.Delete)
+
+	server := http.Server{
+		Addr:    "localhost:3000",
+		Handler: router,
+	}
+
+	err := server.ListenAndServe()
+	helper.PanicIfError(err)
+}
